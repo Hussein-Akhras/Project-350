a3b630107a388d9ae27dfc453d9d410502422488
Spyder Rose <redleader2006@gmail.com>
2026-04-04 01:19:28 +0300

    Initial commit

---
+++ protocol.py
@@ -0,0 +1,12 @@
+import json 
+
+def send_json(sock, data):
+    message = json.dumps(data) + "\n"
+    sock.sendall(message.encode("utf-8"))
+
+
+def receive_json(sock_file):
+    line = sock_file.readline()
+    if line == "":
+        return None
+    return json.loads(line)
---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import sys
+
+from protocol import send_json, receive_json
+
+#listening on all available interfaces#
+
+HOST = "0.0.0.0"
+
+#accepts command line
+PORT = int(sys.argv[1])
+
+
+#clients list#
+clients = {} 
+clients_lock = threading.Lock()  
+
+#match list#
+current_match = None
+match_lock = threading.Lock()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.binf((HOST,PORT))
+server.listen
+
+#main server loop do not touch this or server dies #
+print(f"sever listening on {HOST}:{PORT}")
+
+while True :
+    conn,addr = server.accept
+    print(f"New connection from {addr}")
+
